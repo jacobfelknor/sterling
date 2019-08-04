da2ad02fa92de036a3b94993dee1a17e29f38dd1
Jacob Felknor <jacobfelknor073@gmail.com>
2019-08-04 11:30:35 -0600

    gitignore

--- sterling/settings.py
+++ sterling/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .keys import secret_key, db_password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g(r7qbfriw371%+p35070^ge$ligq=+s^4b&&)^m)*ezlr5vj!'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sterling',
         'USER': 'postgres',
-        'PASSWORD': 'microwave',
+        'PASSWORD': db_password,
         'HOST': 'localhost',
         'PORT': '5432'
     }
