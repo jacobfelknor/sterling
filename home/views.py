d3f1018b2d4072132ea2aa4a9cdf5292f316c2c0
Jacob <jacobfelknor073@gmail.com>
2019-09-09 18:08:29 -0600

    gave charts functionality on dashboard

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from transactions.models import Transaction
+from accounts.models import Account
+from users.models import User
 # Create your views here.
 
 
@@ -8,4 +10,15 @@ def home(request):
 
 def dashboard(request):
     ctx = {}
+    all_accounts = Account.objects.filter(user=request.user)
+    transactions = Transaction.objects.all()
+    categories = set([obj.category for obj in transactions])
+    category_stats = [{category: len(Transaction.objects.filter(category=category))} for category in categories] 
+    net_worth = 0
+    for account in all_accounts:
+        net_worth += account.balance
+    ctx['net_worth'] = net_worth
+    ctx['accounts'] = all_accounts
+    ctx['transactions'] = transactions
+    ctx['category_stats'] = category_stats
     return render(request, 'home/dashboard.html', ctx)
